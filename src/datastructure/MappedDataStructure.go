9689d34593f563c6fa89ad802744207fb8613249
Marco Chilese <marco.chilese@studenti.unipd.it>
2019-07-18 18:01:34 +0200

    Doc update

--- src/datastructure/MappedDataStructure.go
+++ src/datastructure/MappedDataStructure.go
@@ -1,11 +1,12 @@
 package datastructure
 
+// PageContainer represent a list of PageElement, which are page containing complete data about word frequency
 type PageContainer struct {
 	PageList []PageElement
 }
 
+// PageElement represent a page containing complete data about word frequency
 type PageElement struct {
 	PageId uint32
 	Word   map[string]float64
 }
-
